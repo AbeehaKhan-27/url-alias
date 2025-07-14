92283f94077b2c9995e3b5a95b64a85c59ac945a
Abeeha Khan <ak0212@princeton.edu>
2025-07-14 16:22:01 +0700

    refactor: created local alias texts

---
+++ local_alias.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the local_alias. 
+ *
+ * @package   friendly_url 
+ * @copyright 2025, Abeeha Khan 
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string ['pluginname'] = 'Friendly URLs';
+$string ['manage_page'] = 'Manage page';
+$string ['enter_friendly_url'] = 'Enter the friendly URL';
+$string ['enter_destination_url'] = 'Enter the destination URL';
+$string ['friendly_url'] = 'Friendly URL';
+$string ['destination_url'] = 'Destination URL';
+$string ['cancelled_edit_form'] = 'You cancelled the edit URL form';
+$string ['cancelled_filter_form'] = 'You cancelled the filter URL form';
+$string ['create_form'] = 'You created a URL form';
+$string ['update_form'] = 'You updated a URL form';
+$string ['submitted_filter_form'] = 'Filter friendly form';
+$string ['manage_alias'] = 'Manage URLs';
+$string ['edit_alias'] = 'Edit URLs';
+$string ['delete_alias_title'] = 'Delete URL title';
+$string ['delete_alias'] = 'Do you want to delete this URL';
+$string ['delete_alias_button'] = 'Delete URL';
+$string ['err_required'] = 'This field is required';
+$string ['err_invalid'] = 'The alias id is invalid';
+$string ['err_destination'] = 'This destination URL is already taken';
+$string ['err_friendly'] = 'This friendly URL is already taken';
+$string ['query_keyword'] = 'URL filter:';
+$string ['filter_button'] = 'Filter';
+$string ['create_button'] = 'Create URL';
+$string ['Edit_button'] = 'Edit URL';
+$string ['Delete_button'] = 'Delete URL';
+$string ['URL_not_found'] = 'URL not found.';
+?>
