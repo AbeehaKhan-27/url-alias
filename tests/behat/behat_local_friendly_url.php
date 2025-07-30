b77a2640b5f19d352559df41acb8181658af1f67
Abeeha Khan <ak0212@princeton.edu>
2025-07-30 14:49:54 +0700

    refactor: remaining codechecks fixed

--- tests/behat/behat_local_friendly_url.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-use Behat\MinkExtension\Context\MinkContext;
-
-class behat_local_friendly_url extends MinkContext {
-    // You can define step definitions here later if needed.
-}
